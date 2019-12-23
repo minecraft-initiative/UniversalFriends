c88cac2c1f0304cf9e16c026769672448754a50b
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2019-12-24 00:35:13 +0100

    Add basic friend class boilerplate

---
+++ src/main/java/me/ironexception/universal/friends/Friend.java
@@ -0,0 +1,25 @@
+package me.ironexception.universal.friends;
+
+import java.util.UUID;
+
+public class Friend implements IFriend {
+
+    private UUID id;
+    private String name;
+
+    public Friend(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+}
---
+++ src/main/java/me/ironexception/universal/friends/IFriend.java
@@ -0,0 +1,10 @@
+package me.ironexception.universal.friends;
+
+import java.util.UUID;
+
+public interface IFriend {
+
+    String getName();
+    UUID getId();
+
+}
