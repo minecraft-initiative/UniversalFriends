a64772ef8403ba34b8d91ab91ce4f8a8262cc799
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2019-12-24 02:31:40 +0100

    Extend PersonLoaderTest to test value and association And rename everything from friends to persons

--- src/test/java/me/ironexception/universalfriends/json/PersonLoaderTest.java
+++ src/test/java/me/ironexception/universalfriends/json/PersonLoaderTest.java
@@ -1,6 +1,7 @@
 package me.ironexception.universalfriends.json;
 
 import me.ironexception.universalfriends.UniversalFriends;
+import me.ironexception.universalfriends.association.Association;
 import me.ironexception.universalfriends.person.IPerson;
 import org.junit.jupiter.api.Test;
 
@@ -16,21 +17,32 @@ class PersonLoaderTest {
 
     @Test
     void loadFriendConfiguration() throws URISyntaxException {
+        // Construct a loader to load from the friends.json file in the resources directory
         PersonLoader loader = PersonLoader.loader(Paths.get(getClass().getClassLoader().getResource(UniversalFriends.STANDARD_FILE_NAME).toURI()));
+
+        // Make sure no exceptions are thrown while reading the file
         AtomicReference<Configuration<IPerson>> atomicConfiguration = new AtomicReference<>();
         assertDoesNotThrow(() -> {
             atomicConfiguration.set(loader.loadFriendConfiguration());
         });
         Configuration<IPerson> configuration = atomicConfiguration.get();
+
+        // All meta (minimum, maximum) values must be correct
         Meta meta = configuration.getMeta();
         assertEquals(-1, meta.getMinimum(), "Meta minimum value");
         assertEquals(1, meta.getMaximum(), "Meta maximum value");
-        List<IPerson> friendList = configuration.getList();
-        assertNotNull(friendList, "Friend list is nonnull");
-        assertEquals(1, friendList.size());
-        IPerson friend = friendList.get(0);
-        assertEquals(friend.getId(), UUID.fromString("8a2a3ef5-8d27-41b9-a69a-cbb05ac0ed1d"), "UUID is the same");
-        assertEquals(friend.getName(), "foo", "Name is the same");
+
+        // The persons list must exist and be of size 1
+        List<IPerson> personList = configuration.getList();
+        assertNotNull(personList, "Friend list is nonnull");
+        assertEquals(1, personList.size());
+
+        // The sole person in the persons list must have the correct attributes
+        IPerson person = personList.get(0);
+        assertEquals(person.getId(), UUID.fromString("8a2a3ef5-8d27-41b9-a69a-cbb05ac0ed1d"), "UUID is the same");
+        assertEquals(person.getName(), "foo", "Name is the same");
+        assertEquals(0, person.getValue(), "Value is the same");
+        assertEquals(Association.NEUTRAL, person.getAssociation(), "Inferred association is the same");
     }
 
 }
