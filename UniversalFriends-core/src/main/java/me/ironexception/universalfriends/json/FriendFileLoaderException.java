c1d9330c88426ed9a067ca0395c4333c15ca28c2
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2019-12-25 00:22:36 +0100

    Rewrite PersonLoader, now FriendFileLoader

---
+++ UniversalFriends-core/src/main/java/me/ironexception/universalfriends/json/FriendFileLoaderException.java
@@ -0,0 +1,12 @@
+package me.ironexception.universalfriends.json;
+
+public class FriendFileLoaderException extends Exception {
+
+    public FriendFileLoaderException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public FriendFileLoaderException(String s) {
+        super(s);
+    }
+}
