2b278d797650a18e00729b11a6ebca490d4ae40f
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2019-12-25 19:29:44 +0100

    Add utility methods to UniversalFriends, FriendsSet and callbacks

---
+++ UniversalFriends-minecraft/src/main/java/me/ironexception/universalfriends/FriendsSet.java
@@ -0,0 +1,41 @@
+package me.ironexception.universalfriends;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public class FriendsSet extends HashSet<GameProfilePerson> {
+
+    // package-private by design
+    Set<Consumer<GameProfilePerson>> addCallbacks;
+    Set<Consumer<GameProfilePerson>> removeCallbacks;
+
+    public FriendsSet(Collection<? extends GameProfilePerson> c) {
+        super(c);
+
+        addCallbacks = new HashSet<>();
+        removeCallbacks = new HashSet<>();
+    }
+
+    @Override
+    public boolean add(GameProfilePerson gameProfilePerson) {
+        boolean added = super.add(gameProfilePerson);
+        if (added && addCallbacks != null) { // addCallbacks is initialised in the constructor, but addAll (-> add) is called in the superconstructor of this class. Hence, we need to check if addCallbacks has been initialised by now.
+            addCallbacks.forEach(consumer -> consumer.accept(gameProfilePerson));
+        }
+        return added;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        boolean removed = super.remove(o);
+        if (removed) {
+            // Considering we can only add objects of type GameProfilePerson, we assume this cast will succeed.
+            // If anyone did dirty reflection tricks to add something illegal to our set, that's their problem.
+            removeCallbacks.forEach(consumer -> consumer.accept((GameProfilePerson) o));
+        }
+        return super.remove(o);
+    }
+
+}
--- UniversalFriends-minecraft/src/main/java/me/ironexception/universalfriends/UniversalFriends.java
+++ UniversalFriends-minecraft/src/main/java/me/ironexception/universalfriends/UniversalFriends.java
@@ -1,5 +1,6 @@
 package me.ironexception.universalfriends;
 
+import com.mojang.authlib.GameProfile;
 import me.ironexception.universalfriends.configuration.Configuration;
 import me.ironexception.universalfriends.json.Bounds;
 import me.ironexception.universalfriends.json.FriendFileLoader;
@@ -8,6 +9,8 @@ import me.ironexception.universalfriends.json.FriendFileLoaderException;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class UniversalFriends extends Configuration<GameProfilePerson> {
 
@@ -18,7 +21,7 @@ public class UniversalFriends extends Configuration<GameProfilePerson> {
         try {
             UniversalFriends.INSTANCE = FriendFileLoader
                     .<UniversalFriends, GameProfilePerson>loader(GameProfilePerson.class)
-                    .withFactory((bounds, set) -> new UniversalFriends(bounds, new HashSet<>(set)))
+                    .withFactory((bounds, set) -> new UniversalFriends(bounds, new FriendsSet(set)))
                     .withDefaultPath()
                     .load();
         } catch (IOException | FriendFileLoaderException e) {
@@ -34,6 +37,61 @@ public class UniversalFriends extends Configuration<GameProfilePerson> {
 
     private UniversalFriends(Bounds bounds, Set<GameProfilePerson> set) {
         super(bounds, set);
+    }
+
+    /**
+     * Add a person to the friend list
+     * @param profilePerson The {@link GameProfilePerson} to add
+     */
+    public static void addPerson(GameProfilePerson profilePerson) {
+        UniversalFriends.INSTANCE.getFriendList().add(profilePerson);
+    }
+
+    /**
+     * Add a person by their {@link GameProfile}, assuming a friendliness value of <code>0</code> ({@link Standard#STANDARD_NEUTRAL}).
+     * @param profile   The {@link GameProfile} of the person to add
+     */
+    public static void addPerson(GameProfile profile) {
+        GameProfilePerson profilePerson = new GameProfilePerson(profile, Standard.STANDARD_NEUTRAL);
+        addPerson(profilePerson);
+    }
+
+    /**
+     * Remove a person from the friend list
+     * @param person    The {@link GameProfilePerson} to remove
+     * @return          Whether or not this person was in the friend list
+     */
+    public static boolean removePerson(GameProfilePerson person) {
+        return UniversalFriends.INSTANCE.getFriendList().remove(person);
+    }
+
+    /**
+     * Remove a person from the friend list by their {@link GameProfile}
+     * @param profile   The {@link GameProfile} of the person to remove
+     * @return          Whether or not this person was in the friend list
+     */
+    public static boolean removePerson(GameProfile profile) {
+        return UniversalFriends.INSTANCE.getFriendList()
+                .stream()
+                .filter(profilePerson -> profilePerson.getProfile().equals(profile))        // Filter people with equal GameProfiles
+                .map((Function<GameProfilePerson, Object>) UniversalFriends::removePerson)  // Remove them from the friend list
+                .anyMatch(o -> o == Boolean.TRUE);                                          // Return true if any were removed
+    }
+
+    /**
+     * Installs a person add callback, which will be called every time a new person is added to the friend list.
+     * @param consumer  The callback to add
+     */
+    public static void installAddCallback(Consumer<GameProfilePerson> consumer) {
+        ((FriendsSet) UniversalFriends.INSTANCE.getFriendList()).addCallbacks.add(consumer);
+    }
+
+    /**
+     * Installs a person remove callback, which will be called every time a person is removed from the friend list.
+     * @param consumer  The callback to add
+     */
+    public static void installRemoveCallback(Consumer<GameProfilePerson> consumer) {
+        ((FriendsSet) UniversalFriends.INSTANCE.getFriendList()).removeCallbacks.add(consumer);
     }
 
 }
